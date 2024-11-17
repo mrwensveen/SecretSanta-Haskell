6965c75286e6963132abb1ae8dda9f0dc13aec1f
Matthijs Wensveen <matthijs.wensveen@soprasteria.com>
2024-11-18 00:22:21 +0100

    randomChunks function (WIP)

--- src/Lib.hs
+++ src/Lib.hs
@@ -15,4 +15,13 @@ shuffle :: (RandomGen g) => [a] -> g -> [a]
 shuffle xs g =
   let xos = zip xs (randoms g :: [Int])
       sorted = List.sortBy (\(_, a) (_, b) -> compare a b) xos
-   in map fst sorted
+  in map fst sorted
+
+randomChunks :: (RandomGen g) => [a] -> g -> [[a]]
+randomChunks xs gen =
+  let l = length xs
+  in if l <= 3 then [xs] else
+    let (gen1, gen2) = split gen
+        i = randomRExcept (2, l) [l - 1] gen1
+	      (chunk, rest) = List.splitAt i
+	in chunk:(randomChunks rest gen2)
