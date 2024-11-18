ed1686da93fd3a1d1106a4d28c38bec6440271c9
Matthijs Wensveen <matthijs.wensveen@gmail.com>
2024-11-18 01:54:31 +0100

    zip chunks into a Secret pair

--- app/Main.hs
+++ app/Main.hs
@@ -6,9 +6,11 @@ import System.Random
 main :: IO ()
 main = do
   gen <- getStdGen
-  -- print (randomRExcept (2, 4) [3] gen)
   let users = ["Alice", "Bob", "Zeke", "Mariah", "Jim", "Sue", "Cathy", "Tom"]
       (gen1, gen2) = split gen
       shuffled = shuffle users gen1
       chunks = randomChunks shuffled gen2
   print chunks
+  print $ concatMap (\xs -> zipWith Secret (last xs : xs) xs) chunks
+
+data Secret = Secret {giver :: String, reciever :: String} deriving (Show)
