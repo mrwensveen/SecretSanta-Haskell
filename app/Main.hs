f4adc5411a72b356cb47cc4af7f7cb638326f481
Matthijs Wensveen <matthijs.wensveen@gmail.com>
2024-11-18 01:05:37 +0100

    randomChunks implementation

--- app/Main.hs
+++ app/Main.hs
@@ -6,6 +6,9 @@ import System.Random
 main :: IO ()
 main = do
   gen <- getStdGen
+  -- print (randomRExcept (2, 4) [3] gen)
   let users = ["Alice", "Bob", "Zeke", "Mariah", "Jim", "Sue", "Cathy", "Tom"]
-      shuffled = shuffle users gen
-  print shuffled
+      (gen1, gen2) = split gen
+      shuffled = shuffle users gen1
+      chunks = randomChunks shuffled gen2
+  print chunks
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,27 +1,31 @@
 module Lib
-  ( randomRExcept,
-    shuffle,
+  ( shuffle,
+    randomChunks,
+    randomRExcept,
   )
 where
 
 import qualified Data.List as List
 import System.Random
 
-randomRExcept :: (RandomGen g) => (Int, Int) -> [Int] -> g -> Int
-randomRExcept (lo, hi) except gen =
-  head $ filter (`notElem` except) (randomRs (lo, hi) gen)
-
 shuffle :: (RandomGen g) => [a] -> g -> [a]
 shuffle xs g =
   let xos = zip xs (randoms g :: [Int])
       sorted = List.sortBy (\(_, a) (_, b) -> compare a b) xos
-  in map fst sorted
+   in map fst sorted
 
 randomChunks :: (RandomGen g) => [a] -> g -> [[a]]
+randomChunks [] _ = []
 randomChunks xs gen =
   let l = length xs
-  in if l <= 3 then [xs] else
-    let (gen1, gen2) = split gen
-        i = randomRExcept (2, l) [l - 1] gen1
-	      (chunk, rest) = List.splitAt i
-	in chunk:(randomChunks rest gen2)
+   in if l <= 3
+        then [xs]
+        else
+          let (gen1, gen2) = split gen
+              i = randomRExcept (2, l) [l - 1] gen1
+              (chunk, rest) = List.splitAt i xs
+           in chunk : randomChunks rest gen2
+
+randomRExcept :: (RandomGen g) => (Int, Int) -> [Int] -> g -> Int
+randomRExcept (lo, hi) except gen =
+  head $ filter (`notElem` except) (randomRs (lo, hi) gen)
